4587880707c4bbc3e443dd6dc2d2311b4dd5f942
aswath0001 <rsaswaths38@gmail.com>
2025-08-08 15:50:15 +0530

    added security features JWT

--- src/main/java/com/example/CAS/Repository/AdminRepo.java
+++ src/main/java/com/example/CAS/Repository/AdminRepo.java
@@ -1,4 +1,10 @@
 package com.example.CAS.Repository;
+import com.example.CAS.Entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminRepo {
+import java.util.Optional;
+
+public interface AdminRepo extends JpaRepository<Admin,Long> {
+
+    Optional<Admin> findByAdminName(String adminName);
 }
---
+++ src/main/java/com/example/CAS/Service/AdminService.java
@@ -0,0 +1,4 @@
+package com.example.CAS.Service;
+
+public class AdminService {
+}
