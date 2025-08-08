28d5f419f084a833653fe8e0ca410ed32869e5c9
aswath0001 <rsaswaths38@gmail.com>
2025-08-08 16:32:07 +0530

    added security features JWT

--- src/main/java/com/example/CAS/Service/AdminService.java
+++ src/main/java/com/example/CAS/Service/AdminService.java
@@ -1,4 +1,25 @@
 package com.example.CAS.Service;
 
+import com.example.CAS.Entity.Admin;
+import com.example.CAS.Repository.AdminRepo;
+import com.example.CAS.Security.JWTutil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
+@Autowired
+    private AdminRepo adminRepo;
+@Autowired
+    private JWTutil jwTutil;
+
+public String Autenticate(String AdminName,String Password){
+    Admin admin = adminRepo.findByAdminName(AdminName)
+            .orElseThrow(()-> new RuntimeException("Admin not found"));
+    if(!admin.getPassword().equals(Password)){
+        throw new RuntimeException("Invalid Password");
+    }
+    return jwTutil.GenerateToken(AdminName);
+}
+
 }
